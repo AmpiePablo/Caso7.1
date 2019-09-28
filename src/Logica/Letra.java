582431daaecd34f323f17b34bbac9299adf4bbf3
AmpiePablo <39846644+AmpiePablo@users.noreply.github.com>
2019-09-27 21:04:16 -0600

    Version 2

--- src/Logica/Letra.java
+++ src/Logica/Letra.java
@@ -1,5 +1,36 @@
 package Logica;
-
+/**
+ * laita42
+ * @author Pablo Ampie
+ */
 public class Letra {
+    private String letra;
+    private int posibilidadAcierto;
+    
+    Letra(String pLetra){
+        letra=pLetra;
+        posibilidadAcierto=1;
+    }
 
+    public String getLetra() {
+        return letra;
+    }
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+
+    public int getPosibilidadAcierto() {
+        return posibilidadAcierto;
+    }
+
+    public void setPosibilidadAcierto(int posibilidadAcierto) {
+        this.posibilidadAcierto = posibilidadAcierto;
+    }
+    
+   public void sumarPosibilidad(){
+        posibilidadAcierto++;
+    }
+    public void restarPosibilidad(){
+        posibilidadAcierto--;
+    }
 }
